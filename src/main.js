381017132311d8b80c7771f17aee1ac84ff9898e
Moises <mcasaverde@unsa.edu.pe>
2025-03-02 23:31:51 -0500

    initial commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import './assets/main.css'
+import 'primeicons/primeicons.css'
+import router from './router'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import './style.css'
+
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount('#app');
